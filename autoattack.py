8cc39ec11eb6a9faac839bc6631c64198e768f6f
vlad karachev <vladkarachov@gmail.com>
2021-05-27 15:18:20 +0300

    fix

--- autoattack.py
+++ autoattack.py
@@ -5,7 +5,7 @@ from torch.autograd import Variable
 import numpy as np
 import argparse
 import time
-from . import other_utils as utils, autopgd_pt, fab_pt, square, autopgd_pt
+from . import other_utils as utils, autopgd_pt, fab_pt, square
 
 class AutoAttack():
     def __init__(self, model, norm='Linf', eps=.3, seed=None, verbose=True,
@@ -24,20 +24,20 @@ class AutoAttack():
         self.logger = utils.Logger(log_path)
         
         if not self.is_tf_model:
-            from autopgd_pt import APGDAttack
-            self.apgd = APGDAttack(self.model, n_restarts=5, n_iter=100, verbose=False,
+
+            self.apgd = autopgd_pt.APGDAttack(self.model, n_restarts=5, n_iter=100, verbose=False,
                 eps=self.epsilon, norm=self.norm, eot_iter=1, rho=.75, seed=self.seed, device=self.device)
             
-            from fab_pt import FABAttack
-            self.fab = FABAttack(self.model, n_restarts=5, n_iter=100, eps=self.epsilon, seed=self.seed,
+
+            self.fab = fab_pt.FABAttack(self.model, n_restarts=5, n_iter=100, eps=self.epsilon, seed=self.seed,
                 norm=self.norm, verbose=False, device=self.device)
         
-            from square import SquareAttack
-            self.square = SquareAttack(self.model, p_init=.8, n_queries=5000, eps=self.epsilon, norm=self.norm,
+
+            self.square = square.SquareAttack(self.model, p_init=.8, n_queries=5000, eps=self.epsilon, norm=self.norm,
                 n_restarts=1, seed=self.seed, verbose=False, device=self.device, resc_schedule=False)
                 
-            from autopgd_pt import APGDAttack_targeted
-            self.apgd_targeted = APGDAttack_targeted(self.model, n_restarts=1, n_iter=100, verbose=False,
+
+            self.apgd_targeted = autopgd_pt.APGDAttack_targeted(self.model, n_restarts=1, n_iter=100, verbose=False,
                 eps=self.epsilon, norm=self.norm, eot_iter=1, rho=.75, seed=self.seed, device=self.device)
 
         if version in ['standard', 'plus']:
